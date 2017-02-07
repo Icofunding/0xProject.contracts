7c85b98fd3d80a4e863ea63f03d7a0fdee265420
Amir Bandeali <abandeali1@gmail.com>
2017-02-07 08:24:36 -0800

    (feat) Hash fee specs with orderHash separately, and cancelValue to cancel, refactor functions to be under 16 local var limit

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,105 +8,84 @@ contract Exchange is SafeMath {
 
   mapping (bytes32 => uint256) public fills;
 
-  event LogClaimByUser(address indexed maker, address indexed taker, bytes32 indexed hash, address tokenA, address tokenB, uint valueA, uint valueB, uint expiration, uint amountA, uint claimedA);
-  event LogClaimByToken(address maker, address taker, bytes32 hash, address indexed tokenA, address indexed tokenB, uint valueA, uint valueB, uint expiration, uint amountA, uint claimedA);
-  event LogCancel(address indexed maker, bytes32 hash, address indexed tokenA, address indexed tokenB);
+  //need to rethink which params get indexed
+  event LogClaimByUser(address indexed maker, address indexed taker, bytes32 indexed hash, address tokenM, address tokenT, uint valueM, uint valueT, uint expiration, uint amountM, uint claimedM);
+  event LogClaimByToken(address maker, address taker, bytes32 indexed hash, address indexed tokenM, address indexed tokenT, uint valueM, uint valueT, uint expiration, uint amountM, uint claimedM);
+  event LogCancel(address indexed maker, bytes32 hash, address indexed tokenM, address indexed tokenT, uint cancelValue);
 
-  struct Order {
-    address signer;
-    address tokenOffer;
-    uint256 valueOffer;
-    address tokenRequest;
-    uint256 valueRequest;
-    address feeRecipient;
-    uint256 feeValue;
-    uint256 expiration;
-    uint8 v;
-    bytes32 r;
-    bytes32 s;
-  }
+  // addresses = [ maker, tokenM, tokenT, feeRecipient ]
+  // values = [ valueM, valueT, expiration, feeM, feeT ]
+  // why isn't fillValue uint256?
+  function fill(address[4] addresses, uint256[5] values, uint8 v, bytes32 r, bytes32 s, uint fillValue) returns(bool success) {
+    assert(block.timestamp < values[2]);
 
-  // addresses = [ signer, tokenOffer, tokenRequest, feeRecipient ]
-  // values = [ valueOffer, valueRequest, feeValue, expiration ]
-  function fill(address[4] addresses, uint256[4] values, uint8 v, bytes32 r, bytes32 s, uint fillValue) returns(bool success) {
-    Order memory order = formatOrder(addresses, values, v, r, s);
-    bytes32 h = getHash(order);
-    assert(validSignature(order.signer, h, v, r, s));
-    assert(notExpired(order));
-    assert(safeAdd(fills[h], fillValue) <= order.valueOffer);
-    fills[h] = safeAdd(fills[h], fillValue);
-    assert(Token(order.tokenOffer).transferFrom(order.signer, msg.sender, fillValue));
-    assert(Token(order.tokenRequest).transferFrom(msg.sender, order.signer, partialFill(order, fillValue)));
-    // assert(Token(PROTOCOL_TOKEN).transferFrom(order.signer, order.feeRecipient, order.feeValue));
-    // assert(Token(PROTOCOL_TOKEN).transferFrom(msg.sender, order.feeRecipient, order.feeValue));
-    // log events
-    LogClaimByUser(addresses[0], msg.sender, h, addresses[1], addresses[2], values[0], values[1], values[3], fillValue, fills[h]);
-    LogClaimByToken(addresses[0], msg.sender, h, addresses[1], addresses[2], values[0], values[1], values[3], fillValue, fills[h]);
-    return true;
-  }
-
-  function cancel(address[4] addresses, uint256[4] values, uint8 v, bytes32 r, bytes32 s) returns(bool success) {
-    Order memory order = formatOrder(addresses, values, v, r, s);
-    bytes32 h = getHash(order);
-    assert(validSignature(order.signer, h, v, r, s));
-    assert(msg.sender == order.signer);
-    fills[h] = order.valueOffer;
-    // log events
-    LogCancel(addresses[0], h, addresses[1], addresses[2]);
-    return true;
-  }
-
-  // function match(order1, order2) returns(bool success) {}
-  // function test() returns(bool success, string error)
-
-  function formatOrder(address[4] addresses, uint256[4] values, uint8 _v, bytes32 _r, bytes32 _s) internal constant returns(Order) {
-    return Order({
-      signer: addresses[0],
-      tokenOffer: addresses[1],
-      tokenRequest: addresses[2],
-      feeRecipient: addresses[3],
-      valueOffer: values[0],
-      valueRequest: values[1],
-      feeValue: values[2],
-      expiration: values[3],
-      v: _v,
-      r: _r,
-      s: _s
-    });
-  }
-
-  function getHash(Order order) internal constant returns(bytes32) {
-    return sha3(
+    bytes32 orderHash = sha3(
       this,
-      order.signer,
-      order.tokenOffer,
-      order.valueOffer,
-      order.tokenRequest,
-      order.valueRequest,
-      order.feeRecipient,
-      order.feeValue,
-      order.expiration
-      );
+      addresses[0],
+      addresses[1],
+      addresses[2],
+      values[0],
+      values[1],
+      values[2]
+    );
+
+    assert(safeAdd(fills[orderHash], fillValue) <= values[0]);
+
+    bytes32 msgHash = sha3(
+      orderHash,
+      addresses[3],
+      values[3],
+      values[4]
+    );
+
+    assert(validSignature(addresses[0], msgHash, v, r, s));
+
+    assert(Token(addresses[1]).transferFrom(addresses[0], msg.sender, fillValue));
+    assert(Token(addresses[2]).transferFrom(msg.sender, addresses[0], partialFill([values[0], values[1]], fillValue)));
+    assert(Token(PROTOCOL_TOKEN).transferFrom(addresses[0], addresses[3], values[3]));
+    assert(Token(PROTOCOL_TOKEN).transferFrom(msg.sender, addresses[3], values[4]));
+    fills[orderHash] = safeAdd(fills[orderHash], fillValue);
+    // log events
+    LogClaimByUser(addresses[0], msg.sender, orderHash, addresses[1], addresses[2], values[0], values[1], values[3], fillValue, fills[orderHash]);
+    LogClaimByToken(addresses[0], msg.sender, orderHash, addresses[1], addresses[2], values[0], values[1], values[3], fillValue, fills[orderHash]);
+    return true;
   }
 
-  function partialFill(Order order, uint256 fillValue) constant internal returns(uint256) {
-    if (fillValue > order.valueOffer || fillValue == 0) {
+  // addresses = [ maker, tokenM, tokenT ]
+  // values = [ valueM, valueT, expiration ]
+  // should you be able to cancel fee specific orders only?
+  function cancel(address[3] addresses, uint256[3] values, uint cancelValue) returns(bool success) {
+    assert(msg.sender == addresses[0]);
+
+    bytes32 orderHash = sha3(
+      this,
+      addresses[0],
+      addresses[1],
+      addresses[2],
+      values[0],
+      values[1],
+      values[2]
+    );
+
+    fills[orderHash] = safeAdd(fills[orderHash], cancelValue);
+    // log events
+    LogCancel(addresses[0], orderHash, addresses[1], addresses[2], cancelValue);
+    return true;
+  }
+  // values = [ valueM, valueT ]
+  function partialFill(uint256[2] values, uint256 fillValue) constant internal returns(uint256) {
+    if (fillValue > values[0] || fillValue == 0) {
       throw;
     }
     // throw if rounding error > 0.01%
-    if (order.valueRequest < 10**4 && order.valueRequest*fillValue % order.valueOffer != 0) {
+    if (values[1] < 10**4 && values[1] * fillValue % values[0] != 0) {
       throw;
     }
-    return safeMul(fillValue, order.valueRequest) / order.valueOffer;
+    return safeMul(fillValue, values[1]) / values[0];
   }
 
-  function notExpired(Order order) constant internal returns(bool) {
-    return block.timestamp < order.expiration;
-  }
-
-  function validSignature(address signer, bytes32 h, uint8 v, bytes32 r, bytes32 s) constant returns(bool) {
-    return signer == ecrecover(sha3("\x19Ethereum Signed Message:\n32", h), v, r, s);
-    //return signer == ecrecover(h, v, r, s);
+  function validSignature(address maker, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) constant returns(bool) {
+    return maker == ecrecover(sha3("\x19Ethereum Signed Message:\n32", msgHash), v, r, s);
   }
 
   function assert(bool assertion) internal {
