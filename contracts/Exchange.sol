42ef127a6b3ba266d6d2ed1801ccad4e81562fbc
Amir Bandeali <abandeali1@gmail.com>
2017-02-16 16:52:37 -0800

    (feat) Add fill p2p function, and test fill function that accept optional taker

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -9,6 +9,7 @@ contract Exchange is SafeMath {
   mapping (bytes32 => uint256) public fills;
 
   //need to rethink which params get indexed
+  //should p2p fills log different events?
   event LogClaimByUser(
     address indexed maker,
     address indexed taker,
@@ -17,7 +18,7 @@ contract Exchange is SafeMath {
     uint256 valueM,
     uint256 valueT,
     uint256 expiration,
-    bytes32 hash,
+    bytes32 orderHash,
     address indexed feeRecipient,
     uint256 feeM,
     uint256 feeT,
@@ -33,7 +34,7 @@ contract Exchange is SafeMath {
     uint256 valueM,
     uint256 valueT,
     uint256 expiration,
-    bytes32 hash,
+    bytes32 orderHash,
     address feeRecipient,
     uint256 feeM,
     uint256 feeT,
@@ -48,7 +49,7 @@ contract Exchange is SafeMath {
     uint256 valueM,
     uint256 valueT,
     uint256 expiration,
-    bytes32 hash,
+    bytes32 orderHash,
     uint256 cancelValue,
     uint256 remainingValue
   );
@@ -59,6 +60,7 @@ contract Exchange is SafeMath {
   //rs = [r, s]
   function fill(address maker, address feeRecipient, address[2] tokens, uint256[2] values,  uint256[2] fees, uint256 expiration, uint256 fillValue, uint8 v, bytes32[2] rs) returns(bool success) {
    assert(block.timestamp < expiration);
+   assert(fillValue > 0);
 
    bytes32 orderHash = sha3(
      this,
@@ -78,11 +80,19 @@ contract Exchange is SafeMath {
      fees[1]
    ), v, rs[0], rs[1]));
 
+   //TODO: check if values > 0 and check if feeRecipient == '0x0'
    assert(Token(tokens[0]).transferFrom(maker, msg.sender, fillValue));
    assert(Token(tokens[1]).transferFrom(msg.sender, maker, partialFill([values[0], values[1]], fillValue)));
-   assert(Token(PROTOCOL_TOKEN).transferFrom(maker, feeRecipient, fees[0]));
-   assert(Token(PROTOCOL_TOKEN).transferFrom(msg.sender, feeRecipient, fees[1]));
    fills[orderHash] = safeAdd(fills[orderHash], fillValue);
+
+   if (feeRecipient != address(0)) {
+     if (fees[0] > 0) {
+       assert(Token(PROTOCOL_TOKEN).transferFrom(maker, feeRecipient, fees[0]));
+     }
+     if (fees[1] > 0) {
+       assert(Token(PROTOCOL_TOKEN).transferFrom(msg.sender, feeRecipient, fees[1]));
+     }
+   }
 
    // log events
    LogFillEvents([maker, msg.sender, tokens[0], tokens[1], feeRecipient],
@@ -93,6 +103,60 @@ contract Exchange is SafeMath {
    return true;
  }
 
+  /**
+    fill function with optional taker specification
+  **/
+
+  function fillOptionalParams(address[2] traders, address feeRecipient, address[2] tokens, uint256[2] values,  uint256[2] fees, uint256 expiration, uint256 fillValue, uint8 v, bytes32[2] rs) returns(bool success) {
+    assert(block.timestamp < expiration);
+    assert(fillValue > 0);
+
+    if (traders[1] != address(0)) {
+      assert(msg.sender == traders[1]);
+    }
+
+    bytes32 orderHash = sha3(
+      this,
+      traders[0],
+      traders[1],
+      tokens[0],
+      tokens[1],
+      values[0],
+      values[1],
+      expiration
+    );
+
+    assert(safeAdd(fills[orderHash], fillValue) <= values[0]);
+    assert(validSignature(traders[0], sha3(
+      orderHash,
+      feeRecipient,
+      fees[0],
+      fees[1]
+    ), v, rs[0], rs[1]));
+
+    //TODO: check if values > 0 and check if feeRecipient == '0x0'
+    assert(Token(tokens[0]).transferFrom(traders[0], msg.sender, fillValue));
+    assert(Token(tokens[1]).transferFrom(msg.sender, traders[0], partialFill([values[0], values[1]], fillValue)));
+    fills[orderHash] = safeAdd(fills[orderHash], fillValue);
+
+    if (feeRecipient != address(0)) {
+      if (fees[0] > 0) {
+        assert(Token(PROTOCOL_TOKEN).transferFrom(traders[0], feeRecipient, fees[0]));
+      }
+      if (fees[1] > 0) {
+        assert(Token(PROTOCOL_TOKEN).transferFrom(msg.sender, feeRecipient, fees[1]));
+      }
+    }
+
+    // log events
+    LogFillEvents([traders[0], msg.sender, tokens[0], tokens[1], feeRecipient],
+              [values[0], values[1], expiration, fees[0], fees[1], fillValue, values[0] - fills[orderHash]],
+              orderHash
+    );
+
+    return true;
+  }
+
   //addresses = [maker, taker, tokenM, tokenT, feeRecipient]
   //values = [valueM, valueT, expiration, feeM, feeT, fillValue, remainingValue]
   function LogFillEvents(address[5] addresses, uint256[7] values, bytes32 orderHash) {
@@ -100,8 +164,39 @@ contract Exchange is SafeMath {
     LogClaimByToken(addresses[0], addresses[1], addresses[2], addresses[3], values[0], values[1], values[2], orderHash, addresses[4], values[3], values[4], values[5], values[6]);
   }
 
+  //tokens = [tokenM, tokenT]
+  //values = [valueM, valueT]
+  //rs = [r, s]
+  function fillP2P(address maker, address taker, address[2] tokens, uint256[2] values, uint256 expiration, uint256 fillValue, uint8 v, bytes32[2] rs) {
+    assert(msg.sender == taker);
+    assert(fillValue > 0);
+
+    bytes32 orderHash = sha3(
+      this,
+      maker,
+      taker,
+      tokens[0],
+      tokens[1],
+      values[0],
+      values[1],
+      expiration
+    );
+
+    assert(safeAdd(fills[orderHash], fillValue) <= values[0]);
+    assert(validSignature(maker, orderHash, v, rs[0], rs[1]));
+
+    assert(Token(tokens[0]).transferFrom(maker, msg.sender, fillValue));
+    assert(Token(tokens[1]).transferFrom(msg.sender, maker, partialFill([values[0], values[1]], fillValue)));
+    fills[orderHash] = safeAdd(fills[orderHash], fillValue);
+
+    //log events
+
+    return true;
+  }
+
   function cancel(address maker, address[2] tokens, uint256[2] values, uint256 expiration, uint256 cancelValue) returns(bool success) {
     assert(msg.sender == maker);
+    assert(cancelValue > 0);
 
     bytes32 orderHash = sha3(
       this,
@@ -114,7 +209,7 @@ contract Exchange is SafeMath {
     );
 
     fills[orderHash] = safeAdd(fills[orderHash], cancelValue);
-    
+
     // log events
     LogCancel(maker, tokens[0], tokens[1], values[0], values[1], expiration, orderHash, cancelValue, values[0] - fills[orderHash]);
     return true;
