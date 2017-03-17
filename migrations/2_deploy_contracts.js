7590c71e961d8f8b8c71a7520e07e3862a543575
Amir Bandeali <abandeali1@gmail.com>
2017-03-17 13:26:12 -0700

    (feat) Add ExchangeWrapper to deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,21 @@
 const MultiSigWallet = artifacts.require('./MultiSigWallet.sol');
 const Proxy = artifacts.require('./Proxy.sol');
 const Exchange = artifacts.require('./Exchange.sol');
+const ExchangeWrapper = artifacts.require('./ExchangeWrapper.sol');
 const ProtocolToken = artifacts.require('./ProtocolToken.sol');
 const DummyProtocolToken = artifacts.require('./DummyProtocolToken.sol');
 
 module.exports = (deployer, network) => {
   if (network !== 'live') {
-    let accounts = web3.eth.accounts;
+    const accounts = web3.eth.accounts;
     deployer.deploy(MultiSigWallet, [accounts[0], accounts[1]], 2)
     .then(() => deployer.deploy(Proxy))
     .then(() => deployer.deploy(DummyProtocolToken, 0))
     .then(() => deployer.deploy(Exchange, DummyProtocolToken.address, Proxy.address))
+    .then(() => deployer.deploy(ExchangeWrapper, Exchange.address))
     .then(() => Proxy.deployed())
     .then(instance => {
-      let proxy = instance;
+      const proxy = instance;
       proxy.setAuthorization(Exchange.address, true, { from: accounts[0] })
       .then(() => proxy.transferOwnership(MultiSigWallet.address, { from: accounts[0] }));
     });
