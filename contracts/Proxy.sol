fb07d156016f2cdcd149ea3e3b551e632c35289f
Amir Bandeali <abandeali1@gmail.com>
2017-03-22 09:54:15 -0700

    (feat) Add optOut functionality to Proxy

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -7,8 +7,10 @@ contract Proxy is Ownable {
 
   // stores authorized addresses
   mapping (address => bool) public authorities;
+  mapping (address => mapping (address => bool)) public optedOut;
 
-  event LogAuthorizationChange(address indexed target, bool value, address indexed caller);
+  event LogAuthorizationChange(address indexed target, address indexed caller, bool authorized);
+  event LogPersonalAuthorizationChange(address indexed target, address indexed caller, bool optedOut);
 
   // only authorized addresses can invoke functions with this modifier
   modifier onlyAuthorized {
@@ -25,23 +27,39 @@ contract Proxy is Ownable {
     onlyAuthorized
     returns (bool success)
   {
+    if (isOptedOut(msg.sender, _from)) throw;
     if (!Token(_token).transferFrom(_from, _to, _value)) throw;
     return true;
   }
 
-  function setAuthorization(address _target, bool _value)
+  function setAuthorization(address _target, bool _authorized)
     onlyOwner
     returns (bool success)
   {
-    authorities[_target] = _value;
-    LogAuthorizationChange(_target, _value, msg.sender);
+    authorities[_target] = _authorized;
+    LogAuthorizationChange(_target, msg.sender, _authorized);
+    return true;
+  }
+
+  function setPersonalAuthorization(address _target, bool _optedOut)
+    returns (bool success)
+  {
+    optedOut[_target][msg.sender] = _optedOut;
+    LogPersonalAuthorizationChange(_target, msg.sender, _optedOut);
     return true;
   }
 
   function isAuthorized(address _target)
     constant
-    returns (bool _value)
+    returns (bool _authorized)
   {
     return authorities[_target];
+  }
+
+  function isOptedOut(address _target, address _caller)
+    constant
+    returns (bool _optedOut)
+  {
+    return optedOut[_target][_caller];
   }
 }
