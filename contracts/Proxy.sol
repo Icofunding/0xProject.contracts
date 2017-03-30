7cbef87bd6382333bb008fb53f7a4b82c8bc7f97
Amir Bandeali <abandeali1@gmail.com>
2017-03-30 16:03:49 -0700

    (feat) Add comments for AuthDB and Proxy

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -5,13 +5,18 @@ import "./db/AuthDB.sol";
 
 contract Proxy is AuthDB {
 
-  // only authorized addresses can invoke functions with this modifier
+  /// @dev Only authorized addresses can invoke functions with this modifier.
   modifier onlyAuthorized {
     if (!isAddressAuthorized(msg.sender)) throw;
     _;
   }
 
-  // Proxy calls into ERC20 Token contract, invoking transferFrom
+  /// @dev Calls into ERC20 Token contract, invoking transferFrom.
+  /// @param token Address of token to transfer.
+  /// @param from Address to transfer token from.
+  /// @param to Address to transfer token to.
+  /// @param value Amount of token to transfer.
+  /// @return Success of transfer.
   function transferFrom(
     address token,
     address from,
--- contracts/db/AuthDB.sol
+++ contracts/db/AuthDB.sol
@@ -10,6 +10,13 @@ contract AuthDB is Ownable {
   event LogAuthorizedAddressAdded(address indexed target, address indexed caller);
   event LogAuthorizedAddressRemoved(address indexed target, address indexed caller);
 
+  /*
+   * Public functions
+   */
+
+  /// @dev Authorizes an address.
+  /// @param target Address to authorize.
+  /// @return Success of authorization.
   function addAuthorizedAddress(address target)
     onlyOwner
     returns (bool success)
@@ -20,6 +27,9 @@ contract AuthDB is Ownable {
     return true;
   }
 
+  /// @dev Removes authorizion of an address.
+  /// @param target Address to remove authorization from.
+  /// @return Success of deauthorization.
   function removeAuthorizedAddress(address target)
     onlyOwner
     returns (bool success)
@@ -36,6 +46,9 @@ contract AuthDB is Ownable {
     return true;
   }
 
+  /// @dev Authorizes multiple address.
+  /// @param targets Array of addresses to authorize.
+  /// @return Success of authorization.
   function addAuthorizedAddresses(address[] targets)
     onlyOwner
     returns (bool success)
@@ -46,6 +59,9 @@ contract AuthDB is Ownable {
     return true;
   }
 
+  /// @dev Removes authorizion of multiple addresses.
+  /// @param targets Array of addresses to remove authorization from.
+  /// @return Success of deauthorization.
   function removeAuthorizedAddresses(address[] targets)
     onlyOwner
     returns (bool success)
@@ -56,6 +72,13 @@ contract AuthDB is Ownable {
     return true;
   }
 
+  /*
+   * Public constant functions
+   */
+
+  /// @dev Checks if an address is authorized.
+  /// @dev target Address to check authorization of.
+  /// @return Authorization of checked address.
   function isAddressAuthorized(address target)
     constant
     returns (bool isAuthorized)
@@ -63,6 +86,8 @@ contract AuthDB is Ownable {
     return addresses[target];
   }
 
+  /// @dev Gets all authorized addresses.
+  /// @return Array of authorized addresses.
   function getAuthorizedAddresses()
     constant
     returns (address[])
