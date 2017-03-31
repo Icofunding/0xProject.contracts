45fe6ff10b866e5202e0a3632c94fd0d42f3c4cb
Amir Bandeali <abandeali1@gmail.com>
2017-03-30 17:43:46 -0700

    (feat) Add activation threshold to MultiSig

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -10,23 +10,28 @@ contract MultiSigWallet {
     event Confirmation(address indexed sender, uint indexed transactionId);
     event Revocation(address indexed sender, uint indexed transactionId);
     event Submission(uint indexed transactionId);
+    event SubmissionPassed(uint indexed transactionId, uint confirmationTime);
+    event SubmissionDisputed(uint indexed transactionId);
     event Execution(uint indexed transactionId);
     event ExecutionFailure(uint indexed transactionId);
     event Deposit(address indexed sender, uint value);
     event OwnerAddition(address indexed owner);
     event OwnerRemoval(address indexed owner);
     event RequirementChange(uint required);
+    event ActivationThresholdChange(uint activationThreshold);
 
     mapping (uint => Transaction) public transactions;
     mapping (uint => mapping (address => bool)) public confirmations;
     mapping (address => bool) public isOwner;
     address[] public owners;
     uint public required;
+    uint public activationThreshold;
     uint public transactionCount;
 
     struct Transaction {
         address destination;
         uint value;
+        uint confirmationTime;
         bytes data;
         bool executed;
     }
@@ -102,7 +107,8 @@ contract MultiSigWallet {
     /// @dev Contract constructor sets initial owners and required number of confirmations.
     /// @param _owners List of initial owners.
     /// @param _required Number of required confirmations.
-    function MultiSigWallet(address[] _owners, uint _required)
+    /// @param _activationThreshold Duration of time needed after a transaction is confirmed and before it becomes executable, in seconds.
+    function MultiSigWallet(address[] _owners, uint _required, uint _activationThreshold)
         public
         validRequirement(_owners.length, _required)
     {
@@ -113,6 +119,7 @@ contract MultiSigWallet {
         }
         owners = _owners;
         required = _required;
+        activationThreshold = _activationThreshold;
     }
 
     /// @dev Allows to add a new owner. Transaction has to be sent by wallet.
@@ -179,6 +186,16 @@ contract MultiSigWallet {
         RequirementChange(_required);
     }
 
+    /// @dev Changes the activation threshold between when a transaction gets confirmed and becomes executable.
+    /// @param _activationThreshold Duration of time needed after a transaction is confirmed and before it becomes executable, in seconds.
+    function changeActivationThreshold(uint _activationThreshold)
+      public
+      onlyWallet
+    {
+      activationThreshold = _activationThreshold;
+      ActivationThresholdChange(_activationThreshold);
+    }
+
     /// @dev Allows an owner to submit and confirm a transaction.
     /// @param destination Transaction target address.
     /// @param value Transaction ether value.
@@ -202,7 +219,9 @@ contract MultiSigWallet {
     {
         confirmations[transactionId][msg.sender] = true;
         Confirmation(msg.sender, transactionId);
-        executeTransaction(transactionId);
+        if (!isConfirmationTimeSet(transactionId) && isConfirmed(transactionId)) {
+          setConfirmationTime(transactionId, block.timestamp);
+        }
     }
 
     /// @dev Allows an owner to revoke a confirmation for a transaction.
@@ -215,6 +234,9 @@ contract MultiSigWallet {
     {
         confirmations[transactionId][msg.sender] = false;
         Revocation(msg.sender, transactionId);
+        if (isConfirmationTimeSet(transactionId) && !isConfirmed(transactionId)) {
+          setConfirmationTime(transactionId, 0);
+        }
     }
 
     /// @dev Allows anyone to execute a confirmed transaction.
@@ -223,7 +245,8 @@ contract MultiSigWallet {
         public
         notExecuted(transactionId)
     {
-        if (isConfirmed(transactionId)) {
+        // TODO: can delete remove isConfirmed from if statement
+        if (isPastActivationThreshold(transactionId) && isConfirmed(transactionId)) {
             Transaction tx = transactions[transactionId];
             tx.executed = true;
             if (tx.destination.call.value(tx.value)(tx.data))
@@ -247,9 +270,32 @@ contract MultiSigWallet {
         for (uint i = 0; i < owners.length; i++) {
             if (confirmations[transactionId][owners[i]])
                 count += 1;
-            if (count == required)
+            if (count >= required)
                 return true;
         }
+    }
+
+    /// @dev Checks if a transaction's confirmation time has been set.
+    /// @param transactionId Transaction ID.
+    /// @return Status of confirmation time.
+    function isConfirmationTimeSet(uint transactionId)
+      public
+      constant
+      returns (bool)
+    {
+      return transactions[transactionId].confirmationTime != 0;
+    }
+
+    /// @dev Checks if a confirmed submission is executable.
+    /// @param transactionId Transaction ID.
+    /// @return Status of transaction executability.
+    function isPastActivationThreshold(uint transactionId)
+      public
+      constant
+      returns (bool)
+    {
+      uint confirmationTime = transactions[transactionId].confirmationTime;
+      return confirmationTime != 0 && block.timestamp > confirmationTime + activationThreshold;
     }
 
     /*
@@ -269,11 +315,23 @@ contract MultiSigWallet {
         transactions[transactionId] = Transaction({
             destination: destination,
             value: value,
+            confirmationTime: 0,
             data: data,
             executed: false
         });
         transactionCount += 1;
         Submission(transactionId);
+    }
+
+    function setConfirmationTime(uint transactionId, uint confirmationTime)
+      internal
+    {
+      transactions[transactionId].confirmationTime = confirmationTime;
+      if (confirmationTime == 0) {
+        SubmissionDisputed(transactionId);
+      } else {
+        SubmissionPassed(transactionId, confirmationTime);
+      }
     }
 
     /*
@@ -287,7 +345,7 @@ contract MultiSigWallet {
         constant
         returns (uint count)
     {
-        for (uint i=0; i<owners.length; i++)
+        for (uint i = 0; i < owners.length; i++)
             if (confirmations[transactionId][owners[i]])
                 count += 1;
     }
@@ -301,7 +359,7 @@ contract MultiSigWallet {
         constant
         returns (uint count)
     {
-        for (uint i=0; i<transactionCount; i++)
+        for (uint i = 0; i < transactionCount; i++)
             if (   pending && !transactions[i].executed
                 || executed && transactions[i].executed)
                 count += 1;
@@ -360,7 +418,7 @@ contract MultiSigWallet {
                 count += 1;
             }
         _transactionIds = new uint[](to - from);
-        for (i=from; i<to; i++)
+        for (i = from; i < to; i++)
             _transactionIds[i - from] = transactionIdsTemp[i];
     }
 }
