1e1ec4b9b4961c65fe2c3a11942f1f70c99e0038
Amir Bandeali <abandeali1@gmail.com>
2017-04-05 13:41:50 -0700

    Add tokenReg utility file

---
+++ util/tokenRegUtil.js
@@ -0,0 +1,76 @@
+module.exports = tokenReg => {
+  const addToken = (token, { from }) => tokenReg.addToken(
+    token.tokenAddress,
+    token.name,
+    token.symbol,
+    token.url,
+    token.decimals,
+    token.ipfsHash,
+    token.swarmHash,
+    { from }
+  );
+
+  const removeToken = (tokeAddress, { from }) => tokenReg.removeToken(tokenAddress, { from });
+
+  const getTokenMetaData = tokenAddress => {
+    const token = new Promise((resolve, reject) => {
+      tokenReg.getTokenMetaData(tokenAddress).then(data => {
+        const ret = {
+          tokenAddress: data[0],
+          name: data[1],
+          symbol: data[2],
+          url: data[3],
+          decimals: data[4],
+          ipfsHash: data[5],
+          swarmHash: data[6],
+        };
+        resolve(ret);
+      }).catch(e => reject(e));
+    });
+    return token;
+  };
+
+  const getTokenByName = tokenName => {
+    const token = new Promise((resolve, reject) => {
+      tokenReg.getTokenByName(tokenName).then(data => {
+        const ret = {
+          tokenAddress: data[0],
+          name: data[1],
+          symbol: data[2],
+          url: data[3],
+          decimals: data[4],
+          ipfsHash: data[5],
+          swarmHash: data[6],
+        };
+        resolve(ret);
+      }).catch(e => reject(e));
+    });
+    return token;
+  };
+
+  const getTokenBySymbol = tokenSymbol => {
+    const token = new Promise((resolve, reject) => {
+      tokenReg.getTokenBySymbol(tokenSymbol).then(data => {
+        const ret = {
+          tokenAddress: data[0],
+          name: data[1],
+          symbol: data[2],
+          url: data[3],
+          decimals: data[4],
+          ipfsHash: data[5],
+          swarmHash: data[6],
+        };
+        resolve(ret);
+      }).catch(e => reject(e));
+    });
+    return token;
+  };
+
+  return {
+    addToken,
+    removeToken,
+    getTokenMetaData,
+    getTokenByName,
+    getTokenBySymbol,
+  };
+};
