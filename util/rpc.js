3d59ef6486937936d18daea662b58f4deb6908d6
Amir Bandeali <abandeali1@gmail.com>
2017-04-06 09:17:35 -0700

    Add more testrpc helper methods

--- util/rpc.js
+++ util/rpc.js
@@ -6,26 +6,44 @@ const port = truffleConf.networks.development.port;
 
 let id = 0;
 
-exports.increaseTime = time => {
-  const data = JSON.stringify({
+const toPayload = ({ method, params = [] }) => {
+  const payload = JSON.stringify({
     id,
-    method: 'evm_increaseTime',
-    params: [time],
+    method,
+    params,
   });
   id += 1;
+  return payload;
+};
 
-  return new Promise((resolve, reject) => {
+const send = payload => {
+  const ret = new Promise((resolve, reject) => {
     const opts = {
       method: 'POST',
       uri: `http://${host}:${port}`,
-      body: data,
+      body: payload,
     };
     request(opts, (err, body, res) => {
       if (err) {
         reject(err);
-        console.warn('The evm_increaseTime method is only compatible with testrpc');
       }
-      resolve(res);
+      resolve(JSON.parse(res).result);
     });
   });
+  return ret;
+};
+
+exports.snapshot = () => {
+  const payload = toPayload({ method: 'evm_snapshot' });
+  return send(payload);
+};
+
+exports.revert = snapId => {
+  const payload = toPayload({ method: 'evm_revert', params: [snapId] });
+  return send(payload);
+};
+
+exports.increaseTime = time => {
+  const payload = toPayload({ method: 'evm_increaseTime', params: [time] });
+  return send(payload);
 };
