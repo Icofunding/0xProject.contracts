159a3a128fb1acf902d7832d1f56b6d73e6c6b25
willwarren89 <willwarren89@gmail.com>
2017-04-17 13:41:17 -0700

    Apply Apache 2.0 license to Solidity smart contracts.

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -1,10 +1,8 @@
 pragma solidity ^0.4.8;
 
-
 /// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
 /// @author Stefan George - <stefan.george@consensys.net>
-/// @author Amir Bandeali - <amir@0xProject.com>
-
+//  Modified by Amir Bandeali - <amir@0xProject.com>
 contract MultiSigWallet {
 
   uint constant public MAX_OWNER_COUNT = 50;
