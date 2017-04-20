5fa3fb9db9606e89e71d764dc54498f9377031da
Amir Bandeali <abandeali1@gmail.com>
2017-04-20 16:13:47 -0700

    Modify DummyTokenA with more generic interface, delete other DummyTokens

---
+++ contracts/tokens/DummyToken.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.8;
+
+import "./Mintable.sol";
+import "./../base/Ownable.sol";
+
+contract DummyToken is Mintable, Ownable {
+  string public name;
+  string public symbol;
+  uint public decimals;
+
+  function DummyToken(string _name, string _symbol, uint _decimals, uint _totalSupply) {
+    name = _name;
+    symbol = _symbol;
+    decimals = _decimals;
+    totalSupply = _totalSupply;
+    balances[msg.sender] = _totalSupply;
+  }
+
+  function setBalance(address _target, uint _value) onlyOwner {
+    uint currBalance = balanceOf(_target);
+    if (_value < currBalance) {
+      totalSupply -= (currBalance - _value);
+    } else {
+      totalSupply += (_value - currBalance);
+    }
+    balances[_target] = _value;
+  }
+}
