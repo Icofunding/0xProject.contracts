3e7c0b2076037b3109ee34084efe6caa91ac9e6d
Amir Bandeali <abandeali1@gmail.com>
2017-04-21 17:27:37 -0700

    Modify tokens.js and register_tokens in deployment

--- migrations/3_register_tokens.js
+++ migrations/3_register_tokens.js
@@ -1,5 +1,7 @@
 const TokenRegistry = artifacts.require('./TokenRegistry.sol');
+const DummyEtherToken = artifacts.require('./DummyEtherToken.sol');
 const DummyToken = artifacts.require('./DummyToken.sol');
+const ProtocolToken = artifacts.require('./ProtocolToken.sol');
 const tokenData = require('./tokens.js');
 
 module.exports = (deployer, network) => {
@@ -14,8 +16,17 @@ module.exports = (deployer, network) => {
           tokens[sym].symbol,
           tokens[sym].decimals,
           totalSupply
-        ))).then(instances =>
-          Promise.all(instances.map((tokenContract, i) => tokenRegistry.addToken(
+        ))).then(instances => {
+          const weth = {
+            address: DummyEtherToken.address,
+            name: 'ETH Wrapper Token',
+            symbol: 'WETH',
+            url: '',
+            decimals: 18,
+            ipfsHash: '0x0',
+            swarmHash: '0x0',
+          };
+          return Promise.all(instances.map((tokenContract, i) => tokenRegistry.addToken(
             tokenContract.address,
             tokens[tokenSymbols[i]].name,
             tokens[tokenSymbols[i]].symbol,
@@ -23,9 +34,26 @@ module.exports = (deployer, network) => {
             tokens[tokenSymbols[i]].decimals,
             tokens[tokenSymbols[i]].ipfsHash,
             tokens[tokenSymbols[i]].swarmHash
-          )))
-        );
+          )).concat(tokenRegistry.addToken(
+            weth.address,
+            weth.name,
+            weth.symbol,
+            weth.url,
+            weth.decimals,
+            weth.ipfsHash,
+            weth.swarmHash
+          )));
+        });
       }
+      const zrx = {
+        address: ProtocolToken.address,
+        name: '0x Protocol Token',
+        symbol: 'ZRX',
+        url: 'https://www.0xproject.com/',
+        decimals: 18,
+        ipfsHash: '0x0',
+        swarmHash: '0x0',
+      };
       return Promise.all(tokenSymbols.map(sym => tokenRegistry.addToken(
         tokens[sym].address,
         tokens[sym].name,
@@ -34,6 +62,14 @@ module.exports = (deployer, network) => {
         tokens[sym].decimals,
         tokens[sym].ipfsHash,
         tokens[sym].swarmHash
+      )).concat(tokenRegistry.addToken(
+        zrx.address,
+        zrx.name,
+        zrx.symbol,
+        zrx.url,
+        zrx.decimals,
+        zrx.ipfsHash,
+        zrx.swarmHash
       )));
     })
   );
