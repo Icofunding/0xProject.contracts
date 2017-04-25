1ad4994dcc888f4bcbe4bf2412142372b92c749b
Fabio Berger <me@fabioberger.com>
2017-04-25 18:21:44 +0200

    Allow JSON file import support

--- globals.d.ts
+++ globals.d.ts
@@ -2,6 +2,13 @@ declare module 'bn.js';
 declare module 'ethereumjs-abi';
 declare module 'es6-promisify';
 
+declare module '*.json' {
+    const json: any;
+    /* tslint:disable */
+    export default json;
+    /* tslint:enable */
+}
+
 // Truffle injects the following into the global scope
 declare var web3: any; // TODO: figure out how to use Web3 definition from within global.d.ts instead of `any`
 declare var artifacts: any;
