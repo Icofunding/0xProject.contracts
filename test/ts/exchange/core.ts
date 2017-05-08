0196fa1e0e8c80d862d7e0fbe25e5c16a1b96334
Fabio Berger <me@fabioberger.com>
2017-05-08 18:33:12 +0200

    Add missing import

--- test/ts/exchange/core.ts
+++ test/ts/exchange/core.ts
@@ -1,5 +1,6 @@
 import * as assert from 'assert';
 import ethUtil = require('ethereumjs-util');
+import BigNumber = require('bignumber.js');
 import { Balances } from '../../../util/balances';
 import { BNUtil } from '../../../util/bn_util';
 import { ExchangeWrapper } from '../../../util/exchange_wrapper';
