9c0204289c84de8af431927a935c481deecdd0ba
Amir Bandeali <abandeali1@gmail.com>
2017-07-18 19:16:43 -0700

    Update ZRX metadata, fix contructor name

--- contracts/tokens/ZRXToken.sol
+++ contracts/tokens/ZRXToken.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.4.11;
 import "./../base/StandardToken.sol";
 
 contract ZRXToken is StandardToken {
+  
     uint8 constant public decimals = 18;
-    string constant public name = "0x Network Token";
+    string constant public name = "0x Protocol Token";
     string constant public symbol = "ZRX";
 
-    function ProtocolToken() {
-        totalSupply = 10**26; // 100M tokens, 18 decimal places
+    function ZRXToken() {
+        totalSupply = 10**27; // 1 billion tokens, 18 decimal places
         balances[msg.sender] = totalSupply;
     }
 
