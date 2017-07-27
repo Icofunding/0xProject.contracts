2356655f204472d9f1da25d23649dc47e3d1adc3
Amir Bandeali <abandeali1@gmail.com>
2017-07-26 18:39:11 -0700

    Make compiler versions static

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -15,7 +15,7 @@
   limitations under the License.
 
 */
-pragma solidity ^0.4.11;
+pragma solidity 0.4.11;
 
 import "./base/Token.sol";
 import "./base/Ownable.sol";
--- contracts/base/SafeMath.sol
+++ contracts/base/SafeMath.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.11;
 
 contract SafeMath {
     function safeMul(uint a, uint b) internal constant returns (uint) {
