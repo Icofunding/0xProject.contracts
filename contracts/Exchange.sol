36f6fa9202645eadc8b6d5a17198e9430a0ab617
Amir Bandeali <abandeali1@gmail.com>
2017-08-10 17:59:47 -0500

    Update licenses

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,6 +1,6 @@
 /*
 
-  Copyright 2017 ZeroEx Inc.
+  Copyright 2017 ZeroEx Intl.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
   limitations under the License.
 
 */
+
 pragma solidity 0.4.11;
 
 import "./TokenTransferProxy.sol";
