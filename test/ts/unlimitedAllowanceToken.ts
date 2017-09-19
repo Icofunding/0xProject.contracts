e5da702263ca972d63286d7180f2d2c748b1a986
Leonid Logvinov <logvinov.leon@gmail.com>
2017-09-19 16:53:50 +0200

    Migrate unlimitedAllowance tests to zeroEx

--- test/ts/unlimitedAllowanceToken.ts
+++ test/ts/unlimitedAllowanceToken.ts
@@ -3,11 +3,11 @@ import {chaiSetup} from './utils/chai_setup';
 import * as Web3 from 'web3';
 import {ZeroEx} from '0x.js';
 import * as BigNumber from 'bignumber.js';
-import { testUtil } from '../../util/test_util';
-import { Artifacts } from '../../util/artifacts';
-import { ContractInstance } from '../../util/types';
+import {testUtil } from '../../util/test_util';
+import {Artifacts } from '../../util/artifacts';
+import {ContractInstance } from '../../util/types';
 
-const { DummyToken } = new Artifacts(artifacts);
+const {DummyToken } = new Artifacts(artifacts);
 const web3: Web3 = (global as any).web3;
 chaiSetup.configure();
 const expect = chai.expect;
@@ -18,22 +18,23 @@ contract('UnlimitedAllowanceToken', (accounts: string[]) => {
   const spender = accounts[1];
 
   const MAX_MINT_VALUE = new BigNumber(100000000000000000000);
-
+  let tokenAddress: string;
   let token: ContractInstance;
 
   beforeEach(async () => {
-    token = await DummyToken.new({ from: owner });
-    await token.mint(MAX_MINT_VALUE, { from: owner });
+    token = await DummyToken.new({from: owner});
+    await token.mint(MAX_MINT_VALUE, {from: owner});
+    tokenAddress = token.address;
   });
 
   describe('transfer', () => {
     it('should transfer balance from sender to receiver', async () => {
       const receiver = spender;
-      const initOwnerBalance = await token.balanceOf(owner);
+      const initOwnerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
       const amountToTransfer = new BigNumber(1);
-      await token.transfer(receiver, amountToTransfer, { from: owner });
-      const finalOwnerBalance = await token.balanceOf(owner);
-      const finalReceiverBalance = await token.balanceOf(receiver);
+      await zeroEx.token.transferAsync(tokenAddress, owner, receiver, amountToTransfer);
+      const finalOwnerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
+      const finalReceiverBalance = await zeroEx.token.getBalanceAsync(tokenAddress, receiver);
 
       const expectedFinalOwnerBalance = initOwnerBalance.minus(amountToTransfer);
       const expectedFinalReceiverBalance = amountToTransfer;
@@ -42,71 +43,71 @@ contract('UnlimitedAllowanceToken', (accounts: string[]) => {
     });
 
     it('should return true on a 0 value transfer', async () => {
-      const didReturnTrue = await token.transfer.call(spender, 0, { from: owner });
+      const didReturnTrue = await token.transfer.call(spender, 0, {from: owner});
       expect(didReturnTrue).to.be.true();
     });
   });
 
   describe('transferFrom', () => {
     it('should return false if owner has insufficient balance', async () => {
-      const ownerBalance = await token.balanceOf(owner);
+      const ownerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
       const amountToTransfer = ownerBalance.plus(1);
-      await token.approve(spender, amountToTransfer, { from: owner });
-      const didReturnTrue = await token.transferFrom.call(owner, spender, amountToTransfer, { from: spender });
+      await zeroEx.token.transferAsync(tokenAddress, owner, spender, amountToTransfer);
+      const didReturnTrue = await token.transferFrom.call(owner, spender, amountToTransfer, {from: spender});
       expect(didReturnTrue).to.be.false();
     });
 
     it('should return false if spender has insufficient allowance', async () => {
-      const ownerBalance = await token.balanceOf(owner);
+      const ownerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
       const amountToTransfer = ownerBalance;
 
-      const spenderAllowance = await token.allowance(owner, spender);
+      const spenderAllowance = await zeroEx.token.getAllowanceAsync(tokenAddress, owner, spender);
       const spenderAllowanceIsInsufficient = spenderAllowance.cmp(amountToTransfer) < 0;
       expect(spenderAllowanceIsInsufficient).to.be.true();
 
-      const didReturnTrue = await token.transferFrom.call(owner, spender, amountToTransfer, { from: spender });
+      const didReturnTrue = await token.transferFrom.call(owner, spender, amountToTransfer, {from: spender});
       expect(didReturnTrue).to.be.false();
     });
 
     it('should return true on a 0 value transfer', async () => {
       const amountToTransfer = 0;
-      const didReturnTrue = await token.transferFrom.call(owner, spender, amountToTransfer, { from: spender });
+      const didReturnTrue = await token.transferFrom.call(owner, spender, amountToTransfer, {from: spender});
       expect(didReturnTrue).to.be.true();
     });
 
     it('should not modify spender allowance if spender allowance is 2^256 - 1', async () => {
-      const initOwnerBalance = await token.balanceOf(owner);
+      const initOwnerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
       const amountToTransfer = initOwnerBalance;
       const initSpenderAllowance = zeroEx.token.UNLIMITED_ALLOWANCE_IN_BASE_UNITS;
-      await token.approve(spender, initSpenderAllowance, { from: owner });
-      await token.transferFrom(owner, spender, amountToTransfer, { from: spender });
+      await zeroEx.token.transferAsync(tokenAddress, owner, spender, initSpenderAllowance);
+      await zeroEx.token.transferFromAsync(tokenAddress, owner, spender, spender, amountToTransfer);
 
-      const newSpenderAllowance = await token.allowance(owner, spender);
+      const newSpenderAllowance = await zeroEx.token.getAllowanceAsync(tokenAddress, owner, spender);
       expect(initSpenderAllowance).to.be.bignumber.equal(newSpenderAllowance);
     });
 
     it('should transfer the correct balances if spender has sufficient allowance', async () => {
-      const initOwnerBalance = await token.balanceOf(owner);
+      const initOwnerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
       const amountToTransfer = initOwnerBalance;
       const initSpenderAllowance = initOwnerBalance;
-      await token.approve(spender, initSpenderAllowance, { from: owner });
-      await token.transferFrom(owner, spender, amountToTransfer, { from: spender });
+      await zeroEx.token.transferAsync(tokenAddress, owner, spender, initSpenderAllowance);
+      await zeroEx.token.transferFromAsync(tokenAddress, owner, spender, spender, amountToTransfer);
 
-      const newOwnerBalance = await token.balanceOf(owner);
-      const newSpenderBalance = await token.balanceOf(spender);
+      const newOwnerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
+      const newSpenderBalance = await zeroEx.token.getBalanceAsync(tokenAddress, spender);
 
       expect(newOwnerBalance).to.be.bignumber.equal(0);
       expect(newSpenderBalance).to.be.bignumber.equal(initOwnerBalance);
     });
 
     it('should modify allowance if spender has sufficient allowance less than 2^256 - 1', async () => {
-      const initOwnerBalance = await token.balanceOf(owner);
+      const initOwnerBalance = await zeroEx.token.getBalanceAsync(tokenAddress, owner);
       const amountToTransfer = initOwnerBalance;
       const initSpenderAllowance = initOwnerBalance;
-      await token.approve(spender, initSpenderAllowance, { from: owner });
-      await token.transferFrom(owner, spender, amountToTransfer, { from: spender });
+      await zeroEx.token.transferAsync(tokenAddress, owner, spender, initSpenderAllowance);
+      await zeroEx.token.transferFromAsync(tokenAddress, owner, spender, spender, amountToTransfer);
 
-      const newSpenderAllowance = await token.allowance(owner, spender);
+      const newSpenderAllowance = await zeroEx.token.getAllowanceAsync(tokenAddress, owner, spender);
       expect(newSpenderAllowance).to.be.bignumber.equal(0);
     });
   });
