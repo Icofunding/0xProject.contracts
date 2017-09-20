0f175aec5a952fd0233d0a48933cfdfe70a5c850
Leonid Logvinov <logvinov.leon@gmail.com>
2017-09-20 13:46:03 +0200

    Migrate token_transfer_proxy transferFrom

--- test/ts/token_transfer_proxy/transfer_from.ts
+++ test/ts/token_transfer_proxy/transfer_from.ts
@@ -1,10 +1,14 @@
-import * as assert from 'assert';
-import { Balances } from '../../../util/balances';
-import { BNUtil } from '../../../util/bn_util';
-import { testUtil } from '../../../util/test_util';
-import { ContractInstance } from '../../../util/types';
-import { Artifacts } from '../../../util/artifacts';
+import * as chai from 'chai';
+import {chaiSetup} from '../utils/chai_setup';
+import {Balances } from '../../../util/balances';
+import {BNUtil} from '../../../util/bn_util';
+import {testUtil} from '../../../util/test_util';
+import {ContractInstance } from '../../../util/types';
+import {Artifacts} from '../../../util/artifacts';
+import {constants} from '../../../util/constants';
 
+chaiSetup.configure();
+const expect = chai.expect;
 const {
   Exchange,
   TokenTransferProxy,
@@ -12,7 +16,7 @@ const {
   TokenRegistry,
 } = new Artifacts(artifacts);
 
-const { add, sub } = BNUtil;
+const {add, sub} = BNUtil;
 
 contract('TokenTransferProxy', (accounts: string[]) => {
   const INIT_BAL = 100000000;
@@ -36,33 +40,31 @@ contract('TokenTransferProxy', (accounts: string[]) => {
 
     dmyBalances = new Balances([rep], [accounts[0], accounts[1]]);
     await Promise.all([
-      rep.approve(TokenTransferProxy.address, INIT_ALLOW, { from: accounts[0] }),
-      rep.setBalance(accounts[0], INIT_BAL, { from: owner }),
-      rep.approve(TokenTransferProxy.address, INIT_ALLOW, { from: accounts[1] }),
-      rep.setBalance(accounts[1], INIT_BAL, { from: owner }),
+      rep.approve(TokenTransferProxy.address, INIT_ALLOW, {from: accounts[0]}),
+      rep.setBalance(accounts[0], INIT_BAL, {from: owner}),
+      rep.approve(TokenTransferProxy.address, INIT_ALLOW, {from: accounts[1]}),
+      rep.setBalance(accounts[1], INIT_BAL, {from: owner}),
     ]);
   });
 
   describe('transferFrom', () => {
     it('should throw when called by an unauthorized address', async () => {
-      try {
-        await tokenTransferProxy.transferFrom(rep.address, accounts[0], accounts[1], 1000, { from: notAuthorized });
-        throw new Error('tokenTransferProxy.transferFrom succeeded when it should have thrown');
-      } catch (err) {
-        testUtil.assertThrow(err);
-      }
+      expect(tokenTransferProxy.transferFrom(rep.address, accounts[0], accounts[1], 1000, {from: notAuthorized}))
+        .to.be.rejectedWith(constants.INVALID_OPCODE);
     });
 
     it('should allow an authorized address to transfer', async () => {
       const balances = await dmyBalances.getAsync();
 
-      await tokenTransferProxy.addAuthorizedAddress(notAuthorized, { from: owner });
+      await tokenTransferProxy.addAuthorizedAddress(notAuthorized, {from: owner});
       const transferAmt = 10000;
-      await tokenTransferProxy.transferFrom(rep.address, accounts[0], accounts[1], transferAmt, { from: notAuthorized });
+      await tokenTransferProxy.transferFrom(rep.address, accounts[0], accounts[1], transferAmt, {from: notAuthorized});
 
       const newBalances = await dmyBalances.getAsync();
-      assert.equal(newBalances[accounts[0]][rep.address], sub(balances[accounts[0]][rep.address], transferAmt));
-      assert.equal(newBalances[accounts[1]][rep.address], add(balances[accounts[1]][rep.address], transferAmt));
+      expect(newBalances[accounts[0]][rep.address])
+        .to.be.bignumber.equal(sub(balances[accounts[0]][rep.address], transferAmt));
+      expect(newBalances[accounts[1]][rep.address])
+        .to.be.bignumber.equal(sub(balances[accounts[1]][rep.address], transferAmt));
     });
   });
 });
