0d80d7468a0d92452ccc75eef59c4e3465828f56
Leonid Logvinov <logvinov.leon@gmail.com>
2017-09-26 12:12:51 +0200

    Revert signature verification changes

--- util/order.ts
+++ util/order.ts
@@ -23,15 +23,24 @@ export class Order {
       throw new Error('Cannot call isValidSignature on unsigned order');
     }
     const orderHash = this.getOrderHash();
-    const isValidSignature = ZeroEx.isValidSignature(orderHash, this.params as any as ECSignature, this.params.maker);
-    return isValidSignature;
+    const msgHash = ethUtil.hashPersonalMessage(orderHash);
+    try {
+      const pubKey = ethUtil.ecrecover(msgHash, v, ethUtil.toBuffer(r), ethUtil.toBuffer(s));
+      const recoveredAddress = ethUtil.bufferToHex(ethUtil.pubToAddress(pubKey));
+      return recoveredAddress === this.params.maker;
+    } catch (err) {
+      return false;
+    }
   }
   public async signAsync() {
     const orderHash = this.getOrderHash();
-    const signature = await this.zeroEx.signOrderHashAsync(orderHash, this.params.maker);
+    const signature = await promisify(web3.eth.sign)(this.params.maker, orderHash);
+    const {v, r, s} = ethUtil.fromRpcSig(signature);
     this.params = _.assign(this.params, {
       orderHashHex: orderHash,
-      ...signature,
+      v,
+      r: ethUtil.bufferToHex(r),
+      s: ethUtil.bufferToHex(s),
     });
   }
   public createFill(shouldThrowOnInsufficientBalanceOrAllowance?: boolean, fillTakerTokenAmount?: BigNumber.BigNumber) {
